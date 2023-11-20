ce35aaaa87c537e16d9ad0f898dcbf988aa68ec8
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-11-20 18:36:19 +0300

    refac: background color layout

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
       <body>
         <div className="items-start bg-neutral-900 flex flex-col">
           <div className="items-center bg-neutral-900 self-stretch flex w-full flex-col max-md:max-w-full">
-            <div className="bg-[radial-gradient(70.71%_70.71%_at_50%_50%,rgba(255,0,0,0.59)_0%,rgba(0,0,0,0.00)_100%)] self-stretch flex w-full flex-col pt-6 pb-11 px-5 max-md:max-w-full">
+            <div className="bg-[radial-gradient(70.71%_50.71%_at_50%_50%,rgba(255,0,0,0.59)_0%,rgba(0,0,0,0.00)_100%)] self-stretch flex w-full flex-col pt-6 pb-11 px-5 max-md:max-w-full">
               <Header />
               {children}
               <Footer />
