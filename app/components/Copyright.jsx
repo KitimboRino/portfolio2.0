477ece05f87adac21fd3a004d80afdbcd614f92f
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-11-20 18:58:56 +0300

    feat: make copyrigt year dynamic

--- app/components/Copyright.jsx
+++ app/components/Copyright.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 function Copyright() {
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className="items-center self-center flex w-[137px] max-w-full flex-col mt-40 max-md:mt-10">
-            <div className="text-neutral-50 text-base font-light leading-[120%]">KRED Inc © 2023</div>
+            <div className="text-neutral-50 text-base font-light leading-[120%]">KRED Inc © {currentYear}</div>
             <div className="text-zinc-50 text-opacity-60 text-base font-light leading-[140%]">Designed by RINO</div>
         </footer>
     )
