048d3dc29a55c9f328cb931bf78428239baefd0d
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2024-06-11 16:00:51 +0300

    feat: add drift chat

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './components/Header'
 import Copyright from './components/Copyright'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,36 @@ export default function RootLayout({ children }) {
             </div>
           </div>
         </div>
+        <Script id="custom-script" strategy="afterInteractive">
+          {`
+            <!-- Start of Async Drift Code -->
+              "use strict";
+
+              !function() {
+                var t = window.driftt = window.drift = window.driftt || [];
+                if (!t.init) {
+                  if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+                  t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+                  t.factory = function(e) {
+                    return function() {
+                      var n = Array.prototype.slice.call(arguments);
+                      return n.unshift(e), t.push(n), t;
+                    };
+                  }, t.methods.forEach(function(e) {
+                    t[e] = t.factory(e);
+                  }), t.load = function(t) {
+                    var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+                    o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+                    var i = document.getElementsByTagName("script")[0];
+                    i.parentNode.insertBefore(o, i);
+                  };
+                }
+              }();
+              drift.SNIPPET_VERSION = '0.3.1';
+              drift.load('vnrkdgzim6ut');
+              <!-- End of Async Drift Code -->
+          `}
+        </Script>
       </body>
     </html>
   )
