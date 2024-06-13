aa28feff7ae46c8a95777d1704649fff121f761f
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2024-06-13 13:46:13 +0300

    refac(about/paje.js): add cards

--- app/about/page.js
+++ app/about/page.js
@@ -3,6 +3,9 @@ import WorkExperienceCard from "../components/Work"
 import SkillsCard from "../components/Skills"
 import AchievementsCard from "../components/Achievements"
 import SetupCard from "../components/Setup"
+import EducationCard from "../components/Education"
+import PhotoCard from "../components/Photography"
+import PassionCard from "../components/Passion"
 
 export const metadata = {
   title: 'About page',
@@ -11,11 +14,14 @@ export const metadata = {
 export default function About() {
   return (
     <>
-      <AboutHeroSection/>
-      <WorkExperienceCard/>
-      <SkillsCard/>
-      <AchievementsCard/>
-      <SetupCard/>
+      <AboutHeroSection />
+      <WorkExperienceCard />
+      <SkillsCard />
+      <AchievementsCard />
+      <SetupCard />
+      <EducationCard />
+      <PhotoCard />
+      <PassionCard />
     </>
   )
 }
