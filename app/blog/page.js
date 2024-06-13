8c2d7ac1db5f92d2f47c762aa48a8a4f32a43d39
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2024-06-13 14:36:23 +0300

    feat: add comming soon pages

---
+++ app/blog/page.js
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Blog page',
+    description: 'My Blog Posts',
+}
+export default function Blog() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center">
+            <h1 className="text-3xl font-bold mb-4">Coming Soon</h1>
+            <p className="text-lg text-gray-200">The blog page is under construction. Stay tuned!</p>
+        </div>
+    )
+}
---
+++ app/works/page.js
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Works page',
+    description: 'My Works',
+}
+export default function Blog() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center">
+            <h1 className="text-3xl font-bold mb-4">Coming Soon</h1>
+            <p className="text-lg text-gray-200">The works page page is under construction!</p>
+        </div>
+    )
+}
