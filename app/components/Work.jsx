72cac9b463d0508d237d4873fc0289de65cfd056
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2024-06-13 07:59:12 +0300

    refact(WOrk.jsx): card styles

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -32,18 +32,16 @@ const WorkExperienceCard = () => {
     <section className="border border-[color:var(--madiyour-com-mine-shaft,#2E2E2E)] self-stretch bg-neutral-900 flex grow flex-col px-20 py-16 mt-4 rounded-3xl border-solid max-md:max-w-full max-md:px-5">
       <div className="max-w-[1258px] self-center flex w-full flex-col pr-1.5 max-md:max-w-full">
         <h2 className="text-zinc-50 text-opacity-60 text-2xl font-light leading-[120%] self-stretch mt-1 max-md:max-w-full mt-1">Professional life</h2>
-        <h3 className="text-neutral-50 text-3xl font-light leading-[110%] tracking-tighter self-stretch max-md:max-w-full">Work Experience</h3>
+        <h3 className="text-neutral-50 text-3xl font-light leading-[110%] tracking-tighter self-stretch max-md:max-w-full mb-3">Work Experience</h3>
         <ul>
           {workExperiences.map((experience, index) => (
-            <li key={index} className="mb-4">
-              <div className="flex justify-between items-center">
+            <li key={index}>
+              <div className="grid grid-cols-3 gap-4 items-center">
                 <div>
                   <p className="font-semibold">{experience.organization}</p>
                 </div>
-                <div className="text-right">
-                  {/* <p className="text-sm"> */}
-                    <p className="text-sm">{experience.role}</p>
-                  {/* </p> */}
+                <div className="text-left">
+                  <p className="text-sm">{experience.role}</p>
                 </div>
                 <div className="text-right">
                   <p className="text-sm">
@@ -54,6 +52,7 @@ const WorkExperienceCard = () => {
               <br />
             </li>
           ))}
+
         </ul>
       </div>
     </section>
